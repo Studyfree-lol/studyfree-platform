838f49a26e3835b8eb2720a9e7e74aaa1ebbbf0e
Khadim <khadimfall0602@gmail.com>
2024-05-01 21:30:35 +0200

    :)

--- apps/frontend/app/upload/page.tsx
+++ apps/frontend/app/upload/page.tsx
@@ -20,10 +20,7 @@ export default function Search() {
       </Alert>
 
       <div className="flex items-center justify-center w-full">
-        <label
-          for="dropzone-file"
-          className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-        >
+        <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
           <div className="flex flex-col items-center justify-center pt-5 pb-6">
             <UploadCloudIcon className="w-12 h-12 text-gray-400 dark:text-gray-500 mb-2" />
             <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
