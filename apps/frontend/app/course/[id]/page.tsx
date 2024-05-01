d3c030004b5abf26eb8da3fb982a798f2f99f363
Khadim <khadimfall0602@gmail.com>
2024-05-01 19:33:17 +0200

    add stuff

--- apps/frontend/app/course/[id]/page.tsx
+++ apps/frontend/app/course/[id]/page.tsx
@@ -55,6 +55,8 @@ export default function Course() {
         <DocumentCard />
         <DocumentCard />
         <DocumentCard />
+        <DocumentCard />
+        <DocumentCard />
       </div>
     </div>
   );
